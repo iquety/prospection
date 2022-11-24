b90d24362525dcd6b7aa87ec553edd89c5d0ad8b
Ricardo Pereira <contato@ricardopedias.com.br>
2022-11-24 15:52:46 -0300

    Criadas entidades modificadas para testes de unidade

---
+++ tests/Domain/Stream/Support/DummyStreamEntityOtherLabel.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Domain\Stream\Support;
+
+use DateTime;
+use DateTimeImmutable;
+use Iquety\Prospection\Domain\Core\IdentityObject;
+use Tests\Domain\Core\Support\DummyEntity;
+use Tests\Domain\Core\Support\DummyValue;
+
+class DummyStreamEntityOtherLabel extends DummyStreamEntity
+{
+    protected function __construct(
+        private IdentityObject $aggregateId,
+        private string $one,
+        private int $two,
+        private float $three,
+        private DateTimeImmutable $four,
+        private DateTime $five,
+        private DummyValue $six,
+        private DummyEntity $seven
+    ){
+    }
+
+    public static function label(): string
+    {
+        return 'aggregado.teste.custom';
+    }
+}
--- tests/Domain/Stream/Support/DummyStreamEntitySameLabel.php
+++ tests/Domain/Stream/Support/DummyStreamEntitySameLabel.php
@@ -7,11 +7,10 @@ namespace Tests\Domain\Stream\Support;
 use DateTime;
 use DateTimeImmutable;
 use Iquety\Prospection\Domain\Core\IdentityObject;
-use Iquety\Prospection\Domain\Stream\StreamEntity;
 use Tests\Domain\Core\Support\DummyEntity;
 use Tests\Domain\Core\Support\DummyValue;
 
-class DummyStreamEntity extends StreamEntity
+class DummyStreamEntitySameLabel extends DummyStreamEntity
 {
     protected function __construct(
         private IdentityObject $aggregateId,
@@ -28,40 +27,5 @@ class DummyStreamEntity extends StreamEntity
     public static function label(): string
     {
         return 'aggregado.teste';
-    }
-
-    public function one(): string
-    {
-        return $this->one;
-    }
-
-    public function two(): int
-    {
-        return $this->two;
-    }
-
-    public function three(): float
-    {
-        return $this->three;
-    }
-
-    public function four(): DateTimeImmutable
-    {
-        return $this->four;
-    }
-
-    public function five(): DateTime
-    {
-        return $this->five;
-    }
-
-    public function six(): DummyValue
-    {
-        return $this->six;
-    }
-
-    public function seven(): DummyEntity
-    {
-        return $this->seven;
     }
 }
