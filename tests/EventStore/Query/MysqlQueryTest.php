96085210aafe93f952132ccc9c177c40b068a1ab
Ricardo Pereira <contato@ricardopedias.com.br>
2025-04-30 16:45:00 -0300

    Configuração da conexão com banco de dados no CI

--- tests/EventStore/Query/MysqlQueryTest.php
+++ tests/EventStore/Query/MysqlQueryTest.php
@@ -142,11 +142,11 @@ class MysqlQueryTest extends AbstractCase
     public function connectionError(): void
     {
         $connection = new MysqlConnection(
-            'devel',
-            'invalid', // host errado
-            3306,
-            'devel',
-            'devel'
+            (string)getenv('MYSQL_DBNAME'),
+            'invalid', // host errado,
+            (int)getenv('MYSQL_PORT'),
+            (string)getenv('MYSQL_USER'),
+            (string)getenv('MYSQL_PASSWORD')
         );
 
         $object = new MysqlQuery($connection, 'events');
@@ -163,13 +163,7 @@ class MysqlQueryTest extends AbstractCase
     /** @test */
     public function connectionQueryError(): void
     {
-        $connection = new MysqlConnection(
-            'devel',
-            'iquety-prospection-mysql',
-            3306,
-            'devel',
-            'devel'
-        );
+        $connection = $this->connection();
 
         $object = new MysqlQuery($connection, 'not_exists');
 
