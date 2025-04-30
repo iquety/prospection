a263b01fbcbd3555a95cbcd4276c0ce60677dfe6
Ricardo Pereira <contato@ricardopedias.com.br>
2025-04-30 16:40:28 -0300

    Configuração da conexão com banco de dados no CI

--- tests/EventStore/Query/MysqlQueryTest.php
+++ tests/EventStore/Query/MysqlQueryTest.php
@@ -16,11 +16,11 @@ class MysqlQueryTest extends AbstractCase
     private function connection(): MysqlConnection
     {
         return new MysqlConnection(
-            'devel',
-            'iquety-prospection-mysql',
-            3306,
-            'devel',
-            'devel'
+            (string)getenv('MYSQL_DBNAME'),
+            (string)getenv('MYSQL_HOST'),
+            (int)getenv('MYSQL_PORT'),
+            (string)getenv('MYSQL_USER'),
+            (string)getenv('MYSQL_PASSWORD')
         );
     }
 
